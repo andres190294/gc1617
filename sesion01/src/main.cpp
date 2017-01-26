5926bb56ccd9f572fedbf4bbc333ffb9afa78949
Alberto Herrán <alberto.herran@urjc.es>
2017-01-26 16:22:39 +0100

    Sesión 01: Librería GLEW
    En segundo lugar, se inicializa la librería GLEW mediante la función
    "glewInit" y se imprime por pantalla la versión de OpenGL soportada por
    nuestro sistema.

--- sesion01/src/main.cpp
+++ sesion01/src/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <stdio.h>
 
@@ -11,6 +12,13 @@ int main(int argc, char** argv) {
     glutInitWindowSize(500,500);
     glutInitWindowPosition(50,50);
     glutCreateWindow("Sesion 1");
+    
+ // Inicializamos GLEW
+    GLenum err = glewInit();
+    if(GLEW_OK != err) printf("Error: %s\n", glewGetErrorString(err));
+    printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    const GLubyte *oglVersion = glGetString(GL_VERSION);
+    printf("This system supports OpenGL Version: %s\n", oglVersion);
     
     return 0;
 }
