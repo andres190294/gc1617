4d72febdd3dfc2d20cb6da59e892a25c31b31adb
Alberto Herrán <alberto.herran@urjc.es>
2017-01-26 16:20:15 +0100

    Sesión 01: Librería GLUT
    En primer lugar se inicializa la librería GLUT mediante la función
    "glutInit", y tras establecer el modo de dibujado mediante la función
    "glutInitDisplayMode" se crea una ventana sobre la que dibujar la
    escena, indicando su posición y tamaño en pantalla.

--- sesion01/src/main.cpp
+++ sesion01/src/main.cpp
@@ -1,25 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: aherrang
- *
- * Created on 26 de enero de 2017, 15:54
- */
-
-#include <cstdlib>
+#include <GL/freeglut.h>
+#include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
+ // Inicializamos GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(50,50);
+    glutCreateWindow("Sesion 1");
+    
     return 0;
 }
 
