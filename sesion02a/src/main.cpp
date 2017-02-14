ed3f516bc67da9104b4d48e863d97e52ec9fa1cd
Alberto Herrán <alberto.herran@urjc.es>
2017-02-14 12:49:03 +0100

    Sesión 02a: Añadidos los recursos de la sesión 02a
    Los recursos iniciales de esta sesión son una copia de la versión final
    del "main.cpp" de la Sesión 01, donde simplemente se ha modificado el
    color de la tetera a amarillo.

---
+++ sesion02a/src/main.cpp
@@ -0,0 +1,61 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <stdio.h>
+
+void initFunc();
+void funReshape(int w, int h);
+void funDisplay();
+
+int main(int argc, char** argv) {
+
+ // Inicializamos GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(50,50);
+    glutCreateWindow("Sesion 2a");
+    
+ // Inicializamos GLEW
+    GLenum err = glewInit();
+    if(GLEW_OK != err) printf("Error: %s\n", glewGetErrorString(err));
+    printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    const GLubyte *oglVersion = glGetString(GL_VERSION);
+    printf("This system supports OpenGL Version: %s\n", oglVersion);
+    
+ // Inicializaciones específicas
+    initFunc();
+    
+ // Configuración CallBacks
+    glutReshapeFunc(funReshape);
+    glutDisplayFunc(funDisplay);
+    
+ // Bucle principal
+    glutMainLoop();
+    
+    return 0;
+}
+
+void initFunc() {
+    
+}
+
+void funReshape(int w, int h) {
+    
+    printf("Dimensiones %dx%d pixeles\n", w, h);
+    
+}
+
+void funDisplay() {
+    
+ // Borramos el buffer de color
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+ // Dibujamos una tetera con modelo de alambre
+    glColor3f(1.0f, 1.0f, 0.0f);
+	glutWireTeapot(1.0f);
+    
+ // Intercambiamos los buffers
+    glutSwapBuffers();
+    
+}
